5ac13f23ad4609f83b5e858cb0c3e05755b38ebf
Bokeher <bokeher47@gmail.com>
2026-05-06 18:14:38 +0200

    chore: initialize project with Spring Initializr

---
+++ src/main/java/com/bokeher/cinema/CinemaReservationSystem/CinemaReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bokeher.cinema.CinemaReservationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaReservationSystemApplication.class, args);
+	}
+
+}
