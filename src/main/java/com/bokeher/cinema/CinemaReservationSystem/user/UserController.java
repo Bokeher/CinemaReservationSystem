5fdb7fc8cd4abbeb459f2a5ed5f8de67516ff361
Bokeher <bokeher47@gmail.com>
2026-05-10 19:52:14 +0200

    refactor(user): use DTOs in get-by-id and get-by-username endpoints

--- src/main/java/com/bokeher/cinema/CinemaReservationSystem/user/UserController.java
+++ src/main/java/com/bokeher/cinema/CinemaReservationSystem/user/UserController.java
@@ -3,7 +3,6 @@ package com.bokeher.cinema.CinemaReservationSystem.user;
 import com.bokeher.cinema.CinemaReservationSystem.user.dto.RegisterUserRequest;
 import com.bokeher.cinema.CinemaReservationSystem.user.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -19,16 +18,12 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getById(@PathVariable long id) {
-        return userService.findById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public UserResponse getById(@PathVariable long id) {
+        return userService.findById(id);
     }
 
     @GetMapping
-    public ResponseEntity<User> getByUsername(@RequestParam String username) {
-        return userService.findByUsername(username)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public UserResponse getByUsername(@RequestParam String username) {
+        return userService.findByUsername(username);
     }
 }
--- src/main/java/com/bokeher/cinema/CinemaReservationSystem/user/UserService.java
+++ src/main/java/com/bokeher/cinema/CinemaReservationSystem/user/UserService.java
@@ -2,11 +2,10 @@ package com.bokeher.cinema.CinemaReservationSystem.user;
 
 import com.bokeher.cinema.CinemaReservationSystem.user.dto.RegisterUserRequest;
 import com.bokeher.cinema.CinemaReservationSystem.user.dto.UserResponse;
+import com.bokeher.cinema.CinemaReservationSystem.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -29,12 +28,18 @@ public class UserService {
         return userMapper.toResponse(savedUser);
     }
 
-    public Optional<User> findById(long id) {
-        return userRepository.findById(id);
+    public UserResponse findById(long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+
+        return userMapper.toResponse(user);
     }
 
-    public Optional<User> findByUsername(String username) {
-        return userRepository.findByUsername(username);
+    public UserResponse findByUsername(String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException(username));
+
+        return userMapper.toResponse(user);
     }
 
 }
