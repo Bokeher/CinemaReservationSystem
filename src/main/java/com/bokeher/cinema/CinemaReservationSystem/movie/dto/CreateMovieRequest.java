9616b84df883f11f35d46d2b96f8899643237efd
Bokeher <bokeher47@gmail.com>
2026-05-18 05:37:57 +0200

    refactor(movie): remove BaseMovieRequest because  it prevented from partial updates

--- src/main/java/com/bokeher/cinema/CinemaReservationSystem/movie/dto/CreateMovieRequest.java
+++ src/main/java/com/bokeher/cinema/CinemaReservationSystem/movie/dto/CreateMovieRequest.java
@@ -1,9 +1,33 @@
 package com.bokeher.cinema.CinemaReservationSystem.movie.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
-@SuperBuilder
+@Getter
+@Builder
 @NoArgsConstructor
-public class CreateMovieRequest extends BaseMovieRequest {
+@AllArgsConstructor
+public class CreateMovieRequest {
+
+    @NotBlank(message = "Title is required")
+    @Size(max = 255, message = "Title must be at most 255 characters")
+    private String title;
+
+    @Size(max = 1000, message = "Description must be at most 1000 characters")
+    private String description;
+
+    @NotNull(message = "Required age is required")
+    @PositiveOrZero(message = "Required age must be zero or greater")
+    private Integer requiredAge;
+
+    @NotNull(message = "Duration is required")
+    @Positive(message = "Duration must be greater than 0")
+    private Integer durationMinutes;
 }
--- src/main/java/com/bokeher/cinema/CinemaReservationSystem/movie/dto/UpdateMovieRequest.java
+++ src/main/java/com/bokeher/cinema/CinemaReservationSystem/movie/dto/UpdateMovieRequest.java
@@ -1,9 +1,28 @@
 package com.bokeher.cinema.CinemaReservationSystem.movie.dto;
 
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
-@SuperBuilder
+@Getter
+@Builder
 @NoArgsConstructor
-public class UpdateMovieRequest extends BaseMovieRequest {
+@AllArgsConstructor
+public class UpdateMovieRequest {
+
+    @Size(max = 255, message = "Title must be at most 255 characters")
+    private String title;
+
+    @Size(max = 1000, message = "Description must be at most 1000 characters")
+    private String description;
+
+    @PositiveOrZero(message = "Required age must be zero or greater")
+    private Integer requiredAge;
+
+    @Positive(message = "Duration must be greater than 0")
+    private Integer durationMinutes;
 }
