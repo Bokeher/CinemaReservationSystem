57bc9a4b5b3ced1900986c107e2bc7194e7bfe47
Bokeher <bokeher47@gmail.com>
2026-05-21 13:47:03 +0200

    test(movie): refactor and expand MovieService tests

--- src/test/java/com/bokeher/cinema/CinemaReservationSystem/movie/MovieServiceTest.java
+++ src/test/java/com/bokeher/cinema/CinemaReservationSystem/movie/MovieServiceTest.java
@@ -16,9 +16,7 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -50,260 +48,190 @@ class MovieServiceTest {
     }
 
     @Test
-    void shouldFindMovieById() {
-        Movie movie = getSavedMovie();
-
+    void getById_shouldReturnMovie_whenMovieExists() {
+        Movie movie = createExampleMovie();
         when(movieRepository.findById(MOVIE_ID)).thenReturn(Optional.of(movie));
 
-        MovieResponse movieResponse = movieService.findById(MOVIE_ID);
+        Movie result = movieService.getById(MOVIE_ID);
 
-        assertMovieResponse(movieResponse, MOVIE_ID, TITLE, DESCRIPTION, REQUIRED_AGE, DURATION_MINUTES);
+        assertMovie(movie, result);
     }
 
     @Test
-    void shouldThrowWhenMovieByIdNotFound() {
+    void getById_shouldThrowException_whenMovieDoesNotExist() {
         when(movieRepository.findById(MOVIE_ID)).thenReturn(Optional.empty());
 
-        MovieNotFoundException exception = assertThrows(
-                MovieNotFoundException.class,
-                () -> movieService.findById(MOVIE_ID)
-        );
-
-        assertEquals("Movie not found with id: 1", exception.getMessage());
-        verify(movieRepository).findById(MOVIE_ID);
+        assertThrows(MovieNotFoundException.class, () -> movieService.getById(MOVIE_ID));
     }
 
     @Test
-    void shouldFindAllMovies() {
-        when(movieRepository.findAll()).thenReturn(List.of(
-                getSavedMovie(),
-                getUpdatedMovie()
-        ));
+    void findById_shouldReturnMovieResponse_whenMovieExists() {
+        Movie movie = createExampleMovie();
+        when(movieRepository.findById(MOVIE_ID)).thenReturn(Optional.of(movie));
+
+        MovieResponse response = movieService.findById(MOVIE_ID);
+
+        assertResponseEqualsMovie(movie, response);
+    }
+
+    @Test
+    void findAll_shouldReturnListOfResponses_whenMoviesExist() {
+        Movie movie = createExampleMovie();
+        Movie movie2 = createExampleMovie2();
+
+        when(movieRepository.findAll()).thenReturn(List.of(movie, movie2));
 
         List<MovieResponse> movieResponses = movieService.findAll();
 
         assertEquals(2, movieResponses.size());
-        assertSavedMovieResponse(movieResponses.get(0));
-        assertUpdatedMovieResponse(movieResponses.get(1));
-        verify(movieRepository).findAll();
+        assertResponseEqualsMovie(movie, movieResponses.get(0));
+        assertResponseEqualsMovie(movie2, movieResponses.get(1));
     }
 
     @Test
-    void shouldFindOneMovieByTitleIgnoreCase() {
-        String titlePartial = TITLE.substring(0, 3).toLowerCase();
+    void findAll_shouldReturnEmptyList_whenNoMovieExist() {
+        when(movieRepository.findAll()).thenReturn(List.of());
 
-        when(movieRepository.findByTitleContainingIgnoreCase(titlePartial))
-                .thenReturn(List.of(getSavedMovie()));
+        List<MovieResponse> response = movieService.findAll();
 
-        List<MovieResponse> movieResponses = movieService.findByTitleIgnoreCase(titlePartial);
-
-        assertEquals(1, movieResponses.size());
-        assertSavedMovieResponse(movieResponses.get(0));
-
-        verify(movieRepository).findByTitleContainingIgnoreCase(titlePartial);
+        assertTrue(response.isEmpty());
     }
 
     @Test
-    void shouldReturnAllMoviesWhenFindWithoutTitle() {
-        when(movieRepository.findAll()).thenReturn(List.of(
-                getSavedMovie(),
-                getUpdatedMovie()
-        ));
+    void find_shouldReturnAllMovies_whenTitleIsNull() {
+        Movie movie = createExampleMovie();
+        Movie movie2 = createExampleMovie2();
+
+        when(movieRepository.findAll()).thenReturn(List.of(movie, movie2));
 
         List<MovieResponse> movieResponses = movieService.find(null);
 
         assertEquals(2, movieResponses.size());
-        assertSavedMovieResponse(movieResponses.get(0));
-        assertUpdatedMovieResponse(movieResponses.get(1));
+        assertResponseEqualsMovie(movie, movieResponses.get(0));
+        assertResponseEqualsMovie(movie2, movieResponses.get(1));
         verify(movieRepository).findAll();
     }
 
     @Test
-    void shouldReturnMoviesByTitleWhenFindWithTitle() {
+    void find_shouldReturnOneMovie_whenTitleNotNull() {
+        Movie movie = createExampleMovie();
+
         when(movieRepository.findByTitleContainingIgnoreCase(TITLE))
-                .thenReturn(List.of(getSavedMovie()));
+                .thenReturn(List.of(movie));
 
         List<MovieResponse> movieResponses = movieService.find(TITLE);
 
         assertEquals(1, movieResponses.size());
-        assertSavedMovieResponse(movieResponses.get(0));
+        assertResponseEqualsMovie(movie, movieResponses.get(0));
 
         verify(movieRepository).findByTitleContainingIgnoreCase(TITLE);
     }
 
     @Test
-    void shouldCreateMovie() {
+    void createMovie_shouldCreateMovie_whenTitleDoesNotExist() {
         CreateMovieRequest request = getCreateRequest();
-        Movie savedMovie = getSavedMovie();
+        Movie movie = createExampleMovie();
 
         when(movieRepository.existsByTitle(TITLE)).thenReturn(false);
-        when(movieRepository.save(any())).thenReturn(savedMovie);
+        when(movieRepository.save(any())).thenReturn(movie);
 
         MovieResponse response = movieService.createMovie(request);
 
-        assertSavedMovieResponse(response);
+        assertResponseEqualsMovie(movie, response);
 
         ArgumentCaptor<Movie> movieCaptor = ArgumentCaptor.forClass(Movie.class);
 
         verify(movieRepository).existsByTitle(TITLE);
         verify(movieRepository).save(movieCaptor.capture());
 
-        Movie persistedMovie = movieCaptor.getValue();
+        Movie savedMovie = movieCaptor.getValue();
 
         assertAll(
-                () -> assertEquals(TITLE, persistedMovie.getTitle()),
-                () -> assertEquals(DESCRIPTION, persistedMovie.getDescription()),
-                () -> assertEquals(REQUIRED_AGE, persistedMovie.getRequiredAge()),
-                () -> assertEquals(DURATION_MINUTES, persistedMovie.getDuration().toMinutes())
+                () -> assertEquals(TITLE, savedMovie.getTitle()),
+                () -> assertEquals(DESCRIPTION, savedMovie.getDescription()),
+                () -> assertEquals(REQUIRED_AGE, savedMovie.getRequiredAge()),
+                () -> assertEquals(DURATION_MINUTES, savedMovie.getDuration().toMinutes())
         );
     }
 
     @Test
-    void shouldThrowWhenCreatingMovieWithExistingTitle() {
+    void createMovie_shouldThrowException_whenTitleExists() {
         CreateMovieRequest request = getCreateRequest();
 
         when(movieRepository.existsByTitle(TITLE)).thenReturn(true);
 
-        MovieAlreadyExistsException exception = assertThrows(
+        assertThrows(
                 MovieAlreadyExistsException.class,
                 () ->  movieService.createMovie(request)
         );
-
-        assertEquals("Movie with title '" + TITLE + "' already exists", exception.getMessage());
-        verify(movieRepository).existsByTitle(TITLE);
-        verify(movieRepository, never()).save(any());
     }
 
     @Test
-    void shouldDeleteMovie() {
-        Movie movie = getSavedMovie();
-
+    void deleteMovie_shouldDeleteMovie() {
+        Movie movie = createExampleMovie();
         when(movieRepository.findById(MOVIE_ID)).thenReturn(Optional.of(movie));
 
         movieService.deleteMovie(MOVIE_ID);
 
-        verify(movieRepository).findById(MOVIE_ID);
         verify(movieRepository).delete(movie);
     }
 
     @Test
-    void shouldThrowWhenDeletingMovieThatDoesNotExist() {
-        when(movieRepository.findById(MOVIE_ID)).thenReturn(Optional.empty());
-
-        MovieNotFoundException exception = assertThrows(
-                MovieNotFoundException.class,
-                () -> movieService.deleteMovie(MOVIE_ID)
-        );
-
-        assertEquals("Movie not found with id: " + MOVIE_ID, exception.getMessage());
-        verify(movieRepository).findById(MOVIE_ID);
-        verify(movieRepository, never()).delete(any());
-    }
-
-    @Test
-    void shouldUpdateMovie() {
+    void updateMovie_shouldUpdateMovie_whenCompleteRequest() {
         UpdateMovieRequest request = getUpdateRequest();
+        Movie movie = createExampleMovie();
+        Movie updatedMovie = createExampleMovie2();
 
-        when(movieRepository.findById(MOVIE_ID)).thenReturn(Optional.of(getSavedMovie()));
+        when(movieRepository.findById(MOVIE_ID)).thenReturn(Optional.of(movie));
         when(movieRepository.existsByTitle(UPDATED_TITLE)).thenReturn(false);
-        when(movieRepository.save(any())).thenReturn(getUpdatedMovie());
+        when(movieRepository.save(any())).thenReturn(updatedMovie);
 
-        MovieResponse response = movieService.updateMovie(MOVIE_ID, request);
+        MovieResponse responseWithUpdatedMovie = movieService.updateMovie(MOVIE_ID, request);
 
-        assertUpdatedMovieResponse(response);
+        assertResponseEqualsMovie(updatedMovie, responseWithUpdatedMovie);
 
         ArgumentCaptor<Movie> movieCaptor = ArgumentCaptor.forClass(Movie.class);
 
-        verify(movieRepository).findById(MOVIE_ID);
-        verify(movieRepository).existsByTitle(UPDATED_TITLE);
         verify(movieRepository).save(movieCaptor.capture());
 
-        Movie updatedMovie = movieCaptor.getValue();
+        Movie savedMovie = movieCaptor.getValue();
 
         assertAll(
-                () -> assertEquals(UPDATED_TITLE, updatedMovie.getTitle()),
-                () -> assertEquals(UPDATED_DESCRIPTION, updatedMovie.getDescription()),
-                () -> assertEquals(UPDATED_REQUIRED_AGE, updatedMovie.getRequiredAge()),
-                () -> assertEquals(UPDATED_DURATION_MINUTES, updatedMovie.getDuration().toMinutes())
+                () -> assertEquals(UPDATED_TITLE, savedMovie.getTitle()),
+                () -> assertEquals(UPDATED_DESCRIPTION, savedMovie.getDescription()),
+                () -> assertEquals(UPDATED_REQUIRED_AGE, savedMovie.getRequiredAge()),
+                () -> assertEquals(UPDATED_DURATION_MINUTES, savedMovie.getDuration().toMinutes())
         );
     }
 
     @Test
-    void shouldUpdateMovieWithoutCheckingTitleConflictWhenTitleDidNotChange() {
+    void updateMovie_shouldUpdateWithoutCheckingTitleConflict_whenTitleDidNotChange() {
         UpdateMovieRequest request = getUpdateRequestSameTitle();
+        Movie movie = createExampleMovie();
+        Movie updatedMovie = createExampleMovie2();
+        updatedMovie.setTitle(movie.getTitle());
 
-        when(movieRepository.findById(MOVIE_ID)).thenReturn(Optional.of(getSavedMovie()));
-        when(movieRepository.save(any())).thenReturn(getUpdatedMovieSameTitle());
+        when(movieRepository.findById(MOVIE_ID)).thenReturn(Optional.of(movie));
+        when(movieRepository.save(any())).thenReturn(updatedMovie);
 
         MovieResponse response = movieService.updateMovie(MOVIE_ID, request);
 
-        assertUpdatedMovieResponseSameTitle(response);
+        assertResponseEqualsMovie(updatedMovie, response);
 
-        verify(movieRepository).findById(MOVIE_ID);
         verify(movieRepository, never()).existsByTitle(TITLE);
-        verify(movieRepository).save(any());
     }
 
     @Test
-    void shouldThrowWhenUpdatingMissingMovie() {
-        UpdateMovieRequest request = getUpdateRequestSameTitle();
-
-        when(movieRepository.findById(MOVIE_ID)).thenReturn(Optional.empty());
-
-        MovieNotFoundException exception = assertThrows(
-                MovieNotFoundException.class,
-                () -> movieService.updateMovie(MOVIE_ID, request)
-        );
-
-        assertEquals("Movie not found with id: " + MOVIE_ID, exception.getMessage());
-        verify(movieRepository).findById(MOVIE_ID);
-        verify(movieRepository, never()).existsByTitle(TITLE);
-        verify(movieRepository, never()).save(any());
-    }
-
-    @Test
-    void shouldThrowWhenUpdatingMovieWithExistingTitle() {
-        Movie movie = getSavedMovie();
+    void updateMovie_shouldThrowException_whenExistsMovieWithSameTitle() {
+        Movie movie = createExampleMovie();
         UpdateMovieRequest request = getUpdateRequest();
 
         when(movieRepository.findById(MOVIE_ID)).thenReturn(Optional.of(movie));
         when(movieRepository.existsByTitle(UPDATED_TITLE)).thenReturn(true);
 
-        MovieAlreadyExistsException exception = assertThrows(
+        assertThrows(
                 MovieAlreadyExistsException.class,
                 () -> movieService.updateMovie(MOVIE_ID, request)
-        );
-
-        assertEquals("Movie with title '" + UPDATED_TITLE + "' already exists", exception.getMessage());
-        verify(movieRepository).findById(MOVIE_ID);
-        verify(movieRepository).existsByTitle(UPDATED_TITLE);
-        verify(movieRepository, never()).save(any());
-    }
-
-    private void assertSavedMovieResponse(MovieResponse movieResponse) {
-        assertMovieResponse(movieResponse, MOVIE_ID, TITLE, DESCRIPTION, REQUIRED_AGE, DURATION_MINUTES);
-    }
-    private void assertUpdatedMovieResponse(MovieResponse movieResponse) {
-        assertMovieResponse(movieResponse, UPDATED_MOVIE_ID, UPDATED_TITLE, UPDATED_DESCRIPTION, UPDATED_REQUIRED_AGE, UPDATED_DURATION_MINUTES);
-    }
-    private void assertUpdatedMovieResponseSameTitle(MovieResponse movieResponse) {
-        assertMovieResponse(movieResponse, UPDATED_MOVIE_ID, TITLE, UPDATED_DESCRIPTION, UPDATED_REQUIRED_AGE, UPDATED_DURATION_MINUTES);
-    }
-
-    private void assertMovieResponse(
-            MovieResponse response,
-            Long id,
-            String title,
-            String description,
-            int requiredAge,
-            int durationMinutes
-    ) {
-        assertAll(
-                () -> assertEquals(id, response.getId()),
-                () -> assertEquals(title, response.getTitle()),
-                () -> assertEquals(description, response.getDescription()),
-                () -> assertEquals(requiredAge, response.getRequiredAge()),
-                () -> assertEquals(durationMinutes, response.getDurationMinutes())
         );
     }
 
@@ -334,32 +262,43 @@ class MovieServiceTest {
                 .build();
     }
 
-    private Movie getUpdatedMovie() {
-        return new Movie(
+    private Movie createExampleMovie2() {
+        return createMovie(
                 UPDATED_MOVIE_ID,
                 UPDATED_TITLE,
                 UPDATED_DESCRIPTION,
                 UPDATED_REQUIRED_AGE,
-                Duration.ofMinutes(UPDATED_DURATION_MINUTES)
-        );
-    }
-    private Movie getUpdatedMovieSameTitle() {
-        return new Movie(
-                UPDATED_MOVIE_ID,
-                TITLE,
-                UPDATED_DESCRIPTION,
-                UPDATED_REQUIRED_AGE,
-                Duration.ofMinutes(UPDATED_DURATION_MINUTES)
+                UPDATED_DURATION_MINUTES
         );
     }
 
-    private Movie getSavedMovie() {
-        return new Movie(
-                MOVIE_ID,
-                TITLE,
-                DESCRIPTION,
-                REQUIRED_AGE,
-                Duration.ofMinutes(DURATION_MINUTES)
-        );
+    private Movie createExampleMovie() {
+        return createMovie(MOVIE_ID, TITLE, DESCRIPTION, REQUIRED_AGE, DURATION_MINUTES);
+    }
+
+    private Movie createMovie(long id, String title, String description, int requiredAge, int durationMinutes) {
+        return Movie.builder()
+                .id(id)
+                .title(title)
+                .description(description)
+                .requiredAge(requiredAge)
+                .duration(Duration.ofMinutes(durationMinutes))
+                .build();
+    }
+
+    private void assertMovie(Movie expected, Movie actual) {
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getTitle(), actual.getTitle());
+        assertEquals(expected.getDescription(), actual.getDescription());
+        assertEquals(expected.getRequiredAge(), actual.getRequiredAge());
+        assertEquals(expected.getDuration(), actual.getDuration());
+    }
+
+    private void assertResponseEqualsMovie(Movie expectedMovie, MovieResponse actualResponse) {
+        assertEquals(expectedMovie.getId(), actualResponse.getId());
+        assertEquals(expectedMovie.getTitle(), actualResponse.getTitle());
+        assertEquals(expectedMovie.getDescription(), actualResponse.getDescription());
+        assertEquals(expectedMovie.getRequiredAge(), actualResponse.getRequiredAge());
+        assertEquals(expectedMovie.getDuration().toMinutes(), actualResponse.getDurationMinutes());
     }
 }
