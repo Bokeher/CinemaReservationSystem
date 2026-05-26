ecdb88f5f8e0cf22203db5cedbd384e226069dbd
Bokeher <bokeher47@gmail.com>
2026-05-26 06:37:09 +0200

    test(auth): add login integration test

--- src/test/java/com/bokeher/cinema/CinemaReservationSystem/AuthIntegrationTest.java
+++ src/test/java/com/bokeher/cinema/CinemaReservationSystem/AuthIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.bokeher.cinema.CinemaReservationSystem;
 
+import com.bokeher.cinema.CinemaReservationSystem.auth.dto.LoginUserRequest;
 import com.bokeher.cinema.CinemaReservationSystem.auth.dto.RegisterUserRequest;
+import com.bokeher.cinema.CinemaReservationSystem.user.User;
 import com.bokeher.cinema.CinemaReservationSystem.user.UserRepository;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,16 +10,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import java.util.Map;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static com.bokeher.cinema.CinemaReservationSystem.user.UserFixtures.*;
 import static com.bokeher.cinema.CinemaReservationSystem.auth.AuthFixtures.*;
 
 class AuthIntegrationTest extends BaseIntegrationTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @BeforeEach
     void cleanUsers() {
@@ -42,6 +47,32 @@ class AuthIntegrationTest extends BaseIntegrationTest {
         assertThat(body.get("user").get("username").asText()).isEqualTo(USERNAME);
         assertThat(body.get("user").get("email").asText()).isEqualTo(EMAIL);
         assertThat(body.get("user").get("role").asText()).isEqualTo("USER");
+        assertThat(body.get("user").has("password")).isFalse();
+    }
+
+    @Test
+    void login_shouldLoginUserAndReturnToken() throws Exception {
+        User user = userWithoutId()
+                .password(passwordEncoder.encode(PASSWORD))
+                .build();
+
+        userRepository.save(user);
+
+        LoginUserRequest request = loginRequest().build();
+
+        ResponseEntity<String> response = testRestTemplate.postForEntity(
+                "/auth/login",
+                request,
+                String.class
+        );
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        JsonNode body = objectMapper.readTree(response.getBody());
+
+        assertThat(body.get("token").asText()).isNotBlank();
+        assertThat(body.get("user").get("username").asText()).isEqualTo(USERNAME);
+        assertThat(body.get("user").get("email").asText()).isEqualTo(EMAIL);
         assertThat(body.get("user").has("password")).isFalse();
     }
 }
