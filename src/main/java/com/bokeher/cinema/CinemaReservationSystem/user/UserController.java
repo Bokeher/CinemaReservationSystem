b800f95c756d28062105737aaf1413837cb499aa
Bokeher <bokeher47@gmail.com>
2026-05-28 20:03:16 +0200

    feat(users): add /me endpoint for current user

--- src/main/java/com/bokeher/cinema/CinemaReservationSystem/user/UserController.java
+++ src/main/java/com/bokeher/cinema/CinemaReservationSystem/user/UserController.java
@@ -1,14 +1,20 @@
 package com.bokeher.cinema.CinemaReservationSystem.user;
 
+import com.bokeher.cinema.CinemaReservationSystem.security.UserPrincipal;
 import com.bokeher.cinema.CinemaReservationSystem.user.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/me")
 public class UserController {
 
     private final UserService userService;
 
+    @GetMapping
+    public UserResponse getMe(@AuthenticationPrincipal UserPrincipal user) {
+        return userService.findByUsername(user.getUsername());
+    }
 }
